8c8972653ea2625dd0429a16c626eb5d9486ea1e
ujangaripin24 <ujangarifin06@gmail.com>
2023-02-20 19:46:46 +0700

    Ready To Deploy

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  todoList = [{
+    itemName: 'Coding',
+    itemDueDate: '13-02-2022',
+    itemPriority: 'hight',
+    itemCategory: 'work'
+  },{
+    itemName: 'Reading Book',
+    itemDueDate: '11-02-2022',
+    itemPriority: 'low',
+    itemCategory: 'week'
+  },{
+    itemName: 'Sport',
+    itemDueDate: '17-02-2022',
+    itemPriority: 'low',
+    itemCategory: 'week'
+  },{
+    itemName: 'Fishing',
+    itemDueDate: '16-02-2022',
+    itemPriority: 'low',
+    itemCategory: 'week'
+  },]
+
+  itemToday : number = Date.now();
   constructor() {}
 
 }
