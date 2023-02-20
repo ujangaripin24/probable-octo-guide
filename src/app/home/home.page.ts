8c9b7f06cfdc3fa99cd62788fb5eb9b61142965c
ujangaripin24 <ujangarifin06@gmail.com>
2023-02-20 20:35:41 +0700

    add ion-fab button on screen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,22 +9,22 @@ export class HomePage {
 
   todoList = [{
     itemName: 'Coding',
-    itemDueDate: '13-02-2022',
+    itemDueDate: '08-22-2022',
     itemPriority: 'hight',
     itemCategory: 'work'
   },{
     itemName: 'Reading Book',
-    itemDueDate: '11-02-2022',
+    itemDueDate: '08-23-2022',
     itemPriority: 'medium',
     itemCategory: 'week'
   },{
     itemName: 'Sport',
-    itemDueDate: '17-02-2022',
+    itemDueDate: '08-23-2022',
     itemPriority: 'low',
     itemCategory: 'week'
   },{
     itemName: 'Fishing',
-    itemDueDate: '16-02-2022',
+    itemDueDate: '08-23-2022',
     itemPriority: 'low',
     itemCategory: 'week'
   },]
