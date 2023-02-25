b217847fc77247920cfee70d4e1bfb6d583d0b32
ujangaripin24 <ujangarifin06@gmail.com>
2023-02-25 20:58:04 +0700

    savepoint: add card, axios, loading screen to pageLirikLagu

--- src/app/pages/page-lirik-lagu/page-lirik-lagu.page.ts
+++ src/app/pages/page-lirik-lagu/page-lirik-lagu.page.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 import { environment } from 'src/environments/environment';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-page-lirik-lagu',
@@ -11,17 +12,33 @@ import { environment } from 'src/environments/environment';
 export class PageLirikLaguPage implements OnInit {
   DataLaguHot: any;
 
-  constructor(private http:HttpClient ) { }
+  constructor(private http: HttpClient, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
+    this.showLoading();
+
     axios.get(environment.baseUrlAPI + '/hot')
     .then(response => {
       this.DataLaguHot = response.data.data;
       console.log(this.DataLaguHot);
+      this.dismissLoading();
     })
     .catch(error => {
       console.log(error);
     })
   }
 
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading...',
+      spinner: 'circles',
+    });
+
+    await loading.present();
+  }
+
+  async dismissLoading() {
+    await this.loadingCtrl.dismiss();
+  }
 }
+
