9221a728bb3670eac98a88025f16ed5e3ee70552
ujangaripin24 <ujangarifin06@gmail.com>
2023-02-27 20:19:18 +0700

    add multiple marker

--- src/app/pages/page-leaflet/page-leaflet.page.ts
+++ src/app/pages/page-leaflet/page-leaflet.page.ts
@@ -8,6 +8,12 @@ import * as L from 'leaflet';
 })
 export class PageLeafletPage{
   map: L.Map;
+  dataMarkers = [
+    {namaKota: 'Bandung', latitude: -6.957761928333558, longitude: 107.60831075345683},
+    {namaKota: 'Jakarta', latitude: -6.21462, longitude: 106.84513},
+    {namaKota: 'Surabaya', latitude: -7.2575, longitude: 112.7521}
+  ];
+
   constructor() { }
 
   ionViewDidEnter() {
@@ -16,6 +22,9 @@ export class PageLeafletPage{
       attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
       maxZoom: 18,
     }).addTo(this.map);
+    this.dataMarkers.forEach(marker => {
+      const dataMarker = L.marker([marker.latitude, marker.longitude]).addTo(this.map);
+      dataMarker.bindPopup(marker.namaKota);
+    });
   }
-
 }
